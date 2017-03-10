cd09c0c7828e707c010f63e2e6eb7262c9714a7d
Logan Griswold <logan.a.griswold@gmail.com>
2017-03-10 07:28:44 +0000

    added api landing page, moved main route to /whoami

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-app.use('/', function(req, res) {
+app.use('/', express.static(path.join(__dirname ,"out", "request-header-parser-microservice", "1.0.0")));
+app.use('/whoami', function(req, res) {
     var stats = {
         ipaddress: req.headers['x-forwarded-for'],
         language: req.headers['accept-language'].split(',')[0].split(';')[0],
